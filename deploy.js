7e01436ad5cff9d8b2af81561d52bcca9da4cc91
Nicky Harpor <88788515+nickyharpor@users.noreply.github.com>
2026-01-21 20:29:48 +0000

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545/');
 
-const account = web3.eth.accounts.privateKeyToAccount('YOUR_PRIVATE_KEY');
+const account = web3.eth.accounts.privateKeyToAccount('YOUR_PRIVATE_KEY'); // replace YOUR_PRIVATE_KEY
 web3.eth.accounts.wallet.add(account);
 
 const bytecode = fs.readFileSync('./bin/CollectFirstNMw.bin', 'utf8');
@@ -14,7 +14,7 @@ const contract = new web3.eth.Contract(JSON.parse(abi));
 contract.deploy({
     data: bytecode,
     arguments: ['0x3963744012dadf90a9034ea1068f53108b1a3834', // treasury
-                'YOUR_PUBLIC_KEY']
+                'YOUR_PUBLIC_KEY'] // replace YOUR_PUBLIC_KEY
 }).send({
     from: account.address,
     gas: 3000000,
